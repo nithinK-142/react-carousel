b382b8b04265a7342b3e4490de81a1262c74d289
nithinK-142 <nithin.sagar.359@gmail.com>
2023-10-27 11:45:17 +0530

    update: final code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Carousel from "./components/Carousel";
+import { slides } from "./data/carouselData";
 
 function App() {
   return (
-    <main>
-      <Carousel />
+    <main className="app flex-center">
+      <Carousel data={slides}/>
     </main>
   );
 }
---
+++ src/data/carouselData.ts
@@ -0,0 +1,14 @@
+export const slides = [
+  {
+    src: "https://picsum.photos/seed/slide1/600/400",
+    alt: "Slide 1",
+  },
+  {
+    src: "https://picsum.photos/seed/slide2/600/400",
+    alt: "Slide 2",
+  },
+  {
+    src: "https://picsum.photos/seed/slide3/600/400",
+    alt: "Slide 3",
+  },
+];
